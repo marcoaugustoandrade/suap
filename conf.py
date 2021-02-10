680a907e2061410a419027296d45691212f69706
Marco Andrade <marco.andrade@ifro.edu.br>
2021-02-10 14:53:02 -0400

    Delete conf.py

--- conf.py
+++
@@ -1,3 +0,0 @@
-suap_username = "2304730"
-suap_password = "coloque_sua_senha"
-spreadsheet_url = "https://docs.google.com/spreadsheets/d/1VI6PHLmcCgK0SGXF4qxdm5BXoAg6Sg-tvFlF_YgH48U/edit#gid=356176471"
